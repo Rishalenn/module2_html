7ee9c452d9b8d63caca72d3268313a651bc046ca
Rishalenn Pelarija <pelarijarishalenn03@gmail.com>
2026-03-13 06:38:42 -1200

    WSAT-M5: Migrated CV to React

--- personal-cv-react/src/components/About.jsx
+++ personal-cv-react/src/components/About.jsx
@@ -4,20 +4,13 @@ import Card from "./Card";
 function About() {
   return (
     <Card title="About Me">
-
-      <img className="profile" src={profile} alt="Profile photo" />
+      <img src={profile} alt="Profile photo" width="150" />
 
       <p>
         Hi, I'm Rishalenn! I like learning, trying new things, and growing as a person.
       </p>
 
-      <p>
-        Email:{" "}
-        <a href="mailto:pelarijarishalenn03@gmail.com">
-          pelarijarishalenn03@gmail.com
-        </a>
-      </p>
-
+      <p>Email: pelarijarishalenn03@gmail.com</p>
     </Card>
   );
 }
