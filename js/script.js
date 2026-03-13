e89a2709d0f3e7ab56d430ea0a7b60a2fb72b59d
Rishalenn Pelarija <pelarijarishalenn03@gmail.com>
2026-03-12 20:39:53 -1200

    fix toggle dark mode

--- js/script.js
+++ js/script.js
@@ -1,36 +1,52 @@
 console.log("js/script.js connected");
 
+
 const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.textContent = "Toggle Dark Mode";
 
 themeToggle.addEventListener("click", () => {
 
-document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("dark-mode");
 
-if(document.body.classList.contains("dark-mode")){
-    themeToggle.textContent = "Toggle Light Mode";
-}else{
-    themeToggle.textContent = "Toggle Dark Mode";
-}
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "Toggle Light Mode";
+    } else {
+        themeToggle.textContent = "Toggle Dark Mode";
+    }
+
 });
 
-const toggleSkills = document.getElementById("toggleSkills");
 
-const skillsSection = document.getElementById("skillsSection")
+const toggleSkills = document.getElementById("toggleSkills");
+const skillsSection = document.getElementById("skillsSection");
+
 skillsSection.classList.add("hidden");
+
 toggleSkills.addEventListener("click", () => {
     skillsSection.classList.toggle("hidden");
 });
 
 
+
 const submitBtn = document.getElementById("submitBtn");
+
 submitBtn.addEventListener("click", function(event) {
+
     event.preventDefault();
+
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
+
     if (name === "" || email === "") {
+
         alert("Please fill in all required fields.");
+
     } else {
+
         alert("Form submitted successfully!");
         document.getElementById("contactForm").reset();
+
     }
+
 });
