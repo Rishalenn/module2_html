3086896c2bf29190354d72f431848204e1469f5d
Rishalenn Pelarija <pelarijarishalenn03@gmail.com>
2026-03-14 02:10:50 -1200

    WSAT-M5: Migrated CV to React

--- personal-cv-react/vite.config.js
+++ personal-cv-react/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/module2_html/",
+  base: "/module2_html/personal-cv-react/",
   plugins: [react()],
-})
+});
